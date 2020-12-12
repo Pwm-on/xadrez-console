7e3165cd41d1dc288af5205befee25ba1a310ece
Alex Bueno Pereira <Pwm-on@users.noreply.github.com>
2020-12-12 18:55:37 -0300

    Criando a classe posição

---
+++ xadrez-console/Chess-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Chess_console.Board;
+
+namespace Chess_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("Posicao" + p);
+        }
+    }
+}
